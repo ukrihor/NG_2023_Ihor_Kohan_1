3e98b1af977870ff82a9259da0c8878fea649c5c
Ihor Kohan <116275138+LIhorL@users.noreply.github.com>
2023-10-13 20:35:19 +0300

    Changed first task in lection 3

--- Lection1/Task3/main.py
+++ Lection1/Task3/main.py
@@ -1,15 +1,14 @@
-i = 0
-while i == 0:
-    print("If you want to convert Celsius to Farangheit, then enter F\n" + "If you want to convert Farangheit to Celsius, then enter C")
-    Choice = input("Enter (F or C): ")
-    if Choice == "F":
-        user_input = int(input("Input Celsius: "))
-        Fahrenheit = 9/5 * user_input + 32
-        print("He's Fahrenheit: ",Fahrenheit)
-    elif Choice == "C":
-        user_input = int(input(""))
-        Celsius = 5/9 * (user_input - 32)
-        rounded_Celsius = round(Celsius, 1)  
-        print("He's Celsius: ",rounded_Celsius)
-    else:
-        print("Durak? Input F or C")
+
+print("If you want to convert Celsius to Fahrenheit, then enter F\n" + "If you want to convert Fahrenheit to Celsius, then enter C")
+Choice = input("Enter (Fahrenheit or Celsius): ")
+if Choice == "Fahrenheit":
+    user_input = int(input("Input Celsius: "))
+    Fahrenheit = 9/5 * user_input + 32
+    print("He's Fahrenheit: ",Fahrenheit)
+elif Choice == "Celsius":
+    user_input = int(input("Input Fahrenheit: "))
+    Celsius = 5/9 * (user_input - 32)
+    rounded_Celsius = round(Celsius, 1)  
+    print("He's Celsius: ",rounded_Celsius)
+else:
+    print("Error!!!\nPlease input Fahrenheit or Celsius")
