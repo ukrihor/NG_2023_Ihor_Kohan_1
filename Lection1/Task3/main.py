be99a6e8ae2eb1ffd3d6c980ee060ea15930db4f
Ihor Kohan <116275138+LIhorL@users.noreply.github.com>
2023-10-22 18:15:17 +0300

    Completely modified Task3 in Lection1, based on the previous Task3

--- Lection1/Task3/main.py
+++ Lection1/Task3/main.py
@@ -1,14 +1,27 @@
+def menu():
+    print("-"*40)
+    print("Temperature Conversion Program")
+    print("Choose an option:")
+    print("1) Convert from Celsius to Fahrenheit")
+    print("2) Convert from Fahrenheit to Celsius")
+    print("-"*40)
 
-print("If you want to convert Celsius to Fahrenheit, then enter F\n" + "If you want to convert Fahrenheit to Celsius, then enter C")
-Choice = input("Enter (Fahrenheit or Celsius): ")
-if Choice == "Fahrenheit":
-    user_input = int(input("Input Celsius: "))
-    Fahrenheit = 9/5 * user_input + 32
-    print("He's Fahrenheit: ",Fahrenheit)
-elif Choice == "Celsius":
-    user_input = int(input("Input Fahrenheit: "))
-    Celsius = 5/9 * (user_input - 32)
-    rounded_Celsius = round(Celsius, 1)  
-    print("He's Celsius: ",rounded_Celsius)
+def celsius_to_fahrenheit(celsius):
+    return (9/5 * celsius + 32)
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (32 - fahrenheit) * 5/9
+
+menu()
+choice = input("Enter the option number (1/2): ")
+
+if choice == "1":
+    celsius = float(input("Enter the temperature in degrees Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius} degrees Celsius = {fahrenheit} degrees Fahrenheit")
+elif choice == "2":
+    fahrenheit = float(input("Enter the temperature in degrees Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit} degrees Fahrenheit = {celsius} degrees Celsius")
 else:
-    print("Error!!!\nPlease input Fahrenheit or Celsius")
+    print("Invalid choice. Please enter 1 or 2.")
