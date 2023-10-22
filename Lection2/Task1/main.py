397cec5788734a9881b93a052f6d85cb54232f0e
Ihor Kohan <116275138+LIhorL@users.noreply.github.com>
2023-10-22 17:23:39 +0300

    Added Task1 of the Lection2

---
+++ Lection2/Task1/main.py
@@ -0,0 +1,4 @@
+print("-"*10 + "Welcome to my programm" + "-"*10)
+print ("Unique elements: " + str(set(input("Enter your list: ").split())))
+print("-"*42)
+print("Creator: "+"Thank you for using this program!")
